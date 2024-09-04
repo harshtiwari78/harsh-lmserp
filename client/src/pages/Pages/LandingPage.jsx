63be5261fc487fdfaad25662bdabc2d99697673a
harsh <harshtiwari787898@gmail.com>
2024-09-04 08:07:37 -0700

    Initial Commit

---
+++ client/src/pages/Pages/LandingPage.jsx
@@ -0,0 +1,47 @@
+import React, { useState, useEffect } from "react";
+import "../Assets/css/landing.css";
+
+const LandingPage = () => {
+  const [isLoading, setIsLoading] = useState(true);
+
+  useEffect(() => {
+    const timer = setTimeout(() => {
+      setIsLoading(false);
+    }, 3500);
+
+    return () => clearTimeout(timer);
+  }, []);
+
+  const handleImageLoad = () => {
+    setIsLoading(false);
+  };
+
+  return (
+    <div className="land-top">
+      <div className="land-inner-top">
+        <div className={`land-banner-image ${isLoading ? "loading" : ""}`}>
+          {isLoading && (
+            <div className="loaders book">
+              <figure className="page"></figure>
+              <figure className="page"></figure>
+              <figure className="page"></figure>
+            </div>
+          )}  
+        </div>
+        <div className="land-banner-slogan">
+          <div className="land-banner-slogan-inner">
+            <div className="land-logo">lmserp</div>
+            <div className="land-motto">Discover, Learn, Grow</div>
+            <div className="land-button">
+              <a className="landing-button-hover" href="/home">
+                <span>GO!!</span>
+              </a>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default LandingPage;
